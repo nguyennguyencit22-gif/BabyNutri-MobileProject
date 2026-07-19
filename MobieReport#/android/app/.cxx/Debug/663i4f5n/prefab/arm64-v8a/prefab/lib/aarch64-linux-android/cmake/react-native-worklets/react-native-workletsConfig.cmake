a10fb3b06cfb34ad3233ed4dc98c6cf5df055730
NguyenHoangAnhKhoa1609 <khoa.nguyenhoang.cit22@eiu.edu.vn>
2026-07-20 01:22:13 +0700

    chore: remove build artifacts, fix gitignore

--- MobieReport#/android/app/.cxx/Debug/663i4f5n/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/react-native-worklets/react-native-workletsConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET react-native-worklets::worklets)
-add_library(react-native-worklets::worklets SHARED IMPORTED)
-set_target_properties(react-native-worklets::worklets PROPERTIES
-    IMPORTED_LOCATION "C:/CODE/csw430/projectMobile_csw430/BabyNutri-MobileProject/MobieReport#/node_modules/react-native-worklets/android/build/intermediates/cxx/Debug/1d622l1x/obj/arm64-v8a/libworklets.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/CODE/csw430/projectMobile_csw430/BabyNutri-MobileProject/MobieReport#/node_modules/react-native-worklets/android/build/prefab-headers/worklets"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
